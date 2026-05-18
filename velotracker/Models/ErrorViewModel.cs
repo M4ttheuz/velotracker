cd8349f3e39629c3e94d20206c2d07a7a2f7fac3
Mateusz Białek <122973031+M4ttheuz@users.noreply.github.com>
2026-05-18 13:21:07 +0200

    Inital commit

---
+++ velotracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace velotracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
